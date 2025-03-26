b41d9182463b80fd76f4f90ef94f70439fd65a97
wenqiantay <wenqianhello@gmail.com>
2025-03-27 05:14:31 +0800

    added cors

--- App-Server/src/main/java/vttp/miniproj/App_Server/config/WebConfig.java
+++ App-Server/src/main/java/vttp/miniproj/App_Server/config/WebConfig.java
@@ -6,14 +6,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-
     @Override
-    public void addCorsMappings(@SuppressWarnings("null") CorsRegistry registry) {
-
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200") // Allow Angular to communicate with Spring Boot
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed methods
-                .allowedHeaders("*") // Allow all headers
-                .allowCredentials(true); // Allow cookies or credentials (if needed)
+                .allowedOrigins("https://tender-cat-production.up.railway.app")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowCredentials(true);
     }
 }
